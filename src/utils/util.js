19c64ef6a258cbe869695a373d3e602a6dba36f6
Harrison Brown <hpbrown92@gmail.com>
2023-02-01 14:04:38 -0500

    add multiply

--- src/utils/util.js
+++ src/utils/util.js
@@ -5,7 +5,18 @@
  * @param {Number} n2
  */
 function add(n1, n2) {
-  return n1 + n2
+  return n1 + n2;
 }
 
-module.exports.add = add
+/**
+ * Accepts two numbers and returns their product
+ *
+ * @param {Number} n1
+ * @param {Number} n2
+ */
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+module.exports.add = add;
+module.exports.multiply = multiply;
